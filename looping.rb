3cd628f3955b6326a90e09818a3bebe143b24375
Bilal <bilal.ibrahim@student.moringaschool.com>
2022-10-18 15:15:33 +0300

    DONE

--- looping.rb
+++ looping.rb
@@ -1,5 +1,11 @@
 def happy_new_year
   # your code here
+  counter = 10
+while counter > 0
+  puts counter
+  counter -= 1
+end
+puts"Happy New Year!"
 end
 
 # No need to modify this code! Use this to implement the fizzbuzz_printer method.
@@ -17,8 +23,18 @@ end
 
 def fizzbuzz_printer
   # your code here
+  puts (1..100).map {|i|
+  f = i % 3 == 0 ? 'Fizz' : nil
+  b = i % 5 == 0 ? 'Buzz' : nil
+  f || b ? "#{ f }#{ b }" : i
+}
+
 end
 
-def reverse_string(str)
+def reverse_string(string)
   # your code here
+  split_string = string.split("")
+  reversed = []
+  string.size.times { reversed << split_string.pop }
+  reversed.join
 end
